e1d191cc8a24c85707f19d5e3b4ae3dee7288bc0
Zhenyi Qi <zqi27@wisc.edu>
2019-01-04 00:32:12 -0800

    subsetII - iterative method added

--- DFS/SubsetsII.py
+++ DFS/SubsetsII.py
@@ -1,3 +1,8 @@
+# Problem link: https://www.lintcode.com/problem/subsets-ii
+
+# -------- Recursive solution --------
+
+
 class Solution1(object):
     def subsetsWithDup(self, nums):
         self.ans = []
@@ -21,6 +26,38 @@ class Solution1(object):
 # When we're at [1, 2], which means the for loop starts from index 2 (the second 2)
 # Then we only go dfs for the second 2, skip dfs for the third 3.
 
-s = Solution1()
-nums = [1, 1]
+# -------- Iterative solution --------
+
+#                       []
+#                 /      |      \
+#             [1]       [2]     [3]
+#         /    |         |
+#     [1, 2]  [1, 3]    [2, 3]
+#     /
+# [1, 2, 3]
+
+
+class Solution2:
+    """
+    @param nums: A set of numbers.
+    @return: A list of lists. All valid subsets.
+    """
+
+    def subsetsWithDup(self, nums):
+        nums.sort()
+        stack = [([], 0)]
+        subsets = []
+        while stack:
+            for _ in range(len(stack)):
+                subset, start_ind = stack.pop()
+                subsets.append(subset)
+                for i in range(start_ind, len(nums)):
+                    if i > start_ind and nums[i] == nums[i - 1]:
+                        continue
+                    stack.append((subset + [nums[i]], i + 1))
+
+        return subsets
+
+s = Solution2()
+nums = [1, 2, 3]
 print('answer is', s.subsetsWithDup(nums))
